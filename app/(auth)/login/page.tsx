586584f7bdf3a17b1f66c5bd7c415485851a638c
Salman Afzal <salmanafzalozil8@gmail.com>
2025-07-26 03:09:50 +0500

    lOGIN AND SIGNUP FORM ADDED

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { TextField, Box, Button, Typography } from "@mui/material";
 
-function Login() {
+
+export default function Login() {
   return (
-    <div>
-      Login
-    </div>
+    
+    <Box
+
+          component={"form"}
+      // onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+         height:"100vh",
+         backgroundColor:"white",
+        
+      }}>
+            <Typography variant="h3" component="h2" sx={{fontSize:"50px", fontWeight:"bold"}}>
+              LOGIN
+            </Typography>{" "}
+            <TextField id="outlined-basic" label="Username" variant="outlined" />
+            <TextField id="Password" label="Password" variant="outlined" />
+            <Button variant="contained" color="primary" sx={{marginLeft:"130px"}} >Login</Button>
+      
+    </Box>
   )
 }
 
-export default Login
+ 
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,21 +1,32 @@
-import { TextField, Box, Button } from "@mui/material";
+import { TextField, Box, Button, Typography } from "@mui/material";
 import React from "react";
 
 export default function Signup() {
-    const handleSubmit = () =>{
-        console.log("Form submitted");
-    }
+  // const handleSubmit = () =>{
+  //     console.log("Form submitted");
+  // }
   return (
+    
     <Box
-    component={"form"}
-    onSubmit={handleSubmit}
-    sx={{display:"flex" , flexDirection:'column' , gap: 2 , width:300}}
+      component={"form"}
+      // onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+         height:"100vh"
+        
+      }}
     >
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
-        <TextField id="Password" label="Password" variant="outlined" />
-        <TextField id="bio" label="Bio" variant="outlined" />
-        <Button variant="contained">Submit</Button>
-
+      <Typography variant="h3" component="h2" sx={{fontSize:"50px", fontWeight:"bold"}}>
+        SIGN UP
+      </Typography>{" "}
+      <TextField id="outlined-basic" label="Username" variant="outlined" />
+      <TextField id="Password" label="Password" variant="outlined" />
+      <TextField id="bio" label="Bio" variant="outlined" />
+      <Button variant="contained" color="primary" sx={{marginLeft:"130px", }} >Signup</Button>
     </Box>
   );
 }
